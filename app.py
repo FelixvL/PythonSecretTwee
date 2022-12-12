dc3e9847c5825323c5ab81d00c51d138750db868
FelixvL <digitaleservice@gmail.com>
2022-12-12 14:18:06 +0100

    a

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+#from decouple import config
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def hello_world():
+#    VANMIJGEHEIM = config('OOGEHEIM')
+#    geheim = VANMIJGEHEIM
+    geheim = 'hoi'
+    return "<p>Hello, World!</p>" + geheim
